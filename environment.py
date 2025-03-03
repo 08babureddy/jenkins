b2611eb29f72bf0bc058aa5700aebcc1ee45c34e
08babureddy <91196791+08babureddy@users.noreply.github.com>
2025-03-03 14:18:50 +0530

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,3 +1,6 @@
 import os
 def before_all(context):
+   xmlreport_dir_path="results/xml/2024-08-01-11-49-19"
    os.environ["TEST_REPORT_PATH"] = xmlreport_dir_path
+def after_all(context):
+   print("Test execution completed.")
